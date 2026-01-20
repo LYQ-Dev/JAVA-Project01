bc7f367b22747490840648fc464b7186af3ec76d
程翊雪 <1355784761@qq.com>
2026-01-20 17:50:27 +0800

    忽略密钥

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * 这个类是「配置参数容器」，负责把配置文件里的阿里云 OSS 配置读取并存储起来，供其他类（比如 OssConfiguration）使用。
+ */
 @Component
 @ConfigurationProperties(prefix = "sky.alioss")
 @Data
--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -2,8 +2,11 @@ package com.sky.config;
 
 import com.sky.interceptor.JwtTokenAdminInterceptor;
 import com.sky.json.JacksonObjectMapper;
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
