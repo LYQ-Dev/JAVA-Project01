9512067e570ef30810ac3ff98d069a5aeb7c3538
程翊雪 <1355784761@qq.com>
2026-03-07 17:17:49 +0800

    spring task 订单状态定时处理完成

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,11 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -10,4 +15,26 @@ public interface OrderMapper {
      * @param order
      */
     void insert(Orders order);
+
+    /**
+     * 根据状态和下单时间查询订单
+     * @param status
+     * @param orderTime
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrdertimeLT(Integer status, LocalDateTime orderTime);
+
+    /**
+     * 更新订单信息（支持状态、取消原因、取消时间等字段更新）
+     * @param order
+     */
+    @Update("UPDATE orders " +
+            "SET status = #{status}, " +
+            "cancel_reason = #{cancelReason}, " +
+            "cancel_time = #{cancelTime}, " +
+            "update_time = #{updateTime} " +
+            "WHERE id = #{id}")
+    void update(Orders order);
+
+
 }
